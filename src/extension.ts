eeb735354caee34ee078b60c63bc19494414de69
SeungJunLee-Apple <lsj3285007@gmail.com>
2022-03-29 03:58:16 +0900

    updated some code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as helper from './helper';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function createHTML(promt: string) {
+
+	return `the selected text is ${promt}`;
+}
+
+export function getWorld(): string {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		// show error message
+		vscode.window.showErrorMessage('hmmm......');
+		return "N";
+	}
+
+	return editor.document.getText(editor.selection);
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+}
+export function showResult(data: string) {
+	const panel = vscode.window.createWebviewPanel( 'myPanel', 'My Panel', vscode.ViewColumn.One, { enableScripts: true } ); panel.webview.html = `<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' vscode-resource: https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js;"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>My Panel</title> </head> <body> <h1>Hello World! ffjdflkgfdjgfjdgkfdjglk</h1> </body> </html>`;
+}
+export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "codebump" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('codebump.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeBump!');
+		let selectedWord =  getWorld();
+		let search_result = createHTML(selectedWord);
+		showResult(search_result);
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
