0008d8dcda7a81efd1c64e7f122197d9474d4ebd
seungjun-green <lsj3285007@gmail.com>
2022-03-31 05:01:40 +0900

    updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,98 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as helper from './helper';
 
-export function createHTML(promt: string) {
 
-	return `the selected text is ${promt}`;
-}
 
-export function getWorld(): string {
+export function modifyText(type: string) {
 	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		// show error message
-		vscode.window.showErrorMessage('hmmm......');
-		return "N";
-	}
-	return editor.document.getText(editor.selection);
+        if (!editor) {
+            // show error message
+            vscode.window.showErrorMessage('No Text was selected');
+            return;
+        } else {
+			const curr = editor.document.getText(editor.selection);
+			const position = editor.selection.active;
+			console.log(position);
+			let a = position.line;
+			let b = position.character;
+	
+			editor.edit(editBuilder => {
+				editBuilder.insert(new vscode.Position(a,b), type);
+			});
+		}
 }
-
-export function showResult(data: string) {
-	const panel = vscode.window.createWebviewPanel( 'myPanel', 'My Panel', vscode.ViewColumn.One, { enableScripts: true } ); panel.webview.html = `<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' vscode-resource: https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js;"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>My Panel</title> </head> <body> <h1>Hello World! ffjdflkgfdjgfjdgkfdjglk</h1> </body> </html>`;
-}
-
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "codebump" is now active!');
 	let disposable = vscode.commands.registerCommand('codebump.helloWorld', () => {
-		vscode.window.showInformationMessage("Launching CodeBump ...");
-		
+		vscode.window.showInformationMessage("Welcome to CodeBump.");	
 	});
 
-	let disposable2 = vscode.commands.registerCommand('codebump.helloWorld2', () => {
-
-		vscode.window.showInformationMessage("Searching about the function ... ");
-
-		let selectedWord =  getWorld();
-		let searchresult = createHTML(selectedWord);
-		showResult(searchresult);
+	let largestHeading = vscode.commands.registerCommand('codebump.largestHeading', () => {
+		vscode.window.showInformationMessage("Formatted into Largest Heading ...");
+		modifyText("#");
 	});
+
+	let secondHeading = vscode.commands.registerCommand('codebump.secondHeading', () => {
+		vscode.window.showInformationMessage("Formatted into second largest heading ...");
+		modifyText("##");
+	});
+
+	let smallHeading = vscode.commands.registerCommand('codebump.smallHeading', () => {
+		vscode.window.showInformationMessage("Formatted into smallest heading ...");
+		modifyText("###");
+	});
+
+	let boldText = vscode.commands.registerCommand('codebump.boldText', () => {
+		vscode.window.showInformationMessage("Formatted into Bold text ...");
+		modifyText("**");
+	});
+
+	let italic = vscode.commands.registerCommand('codebump.italic', () => {
+		vscode.window.showInformationMessage("Formatted into italic text ...");
+		modifyText("* *");
+	});
+
+	let strikethrough = vscode.commands.registerCommand('codebump.strikethrough', () => {
+		vscode.window.showInformationMessage("Formatted into Strikethrough text ...");
+		modifyText("~~ ~~");
+	});
+
+	let boldItalic = vscode.commands.registerCommand('codebump.boldItalic', () => {
+		vscode.window.showInformationMessage("Formatted into Bold and nested italic text ...");
+		modifyText("** **");
+	});
+
+	let allBoldItalic = vscode.commands.registerCommand('codebump.allBoldItalic', () => {
+		vscode.window.showInformationMessage("Formatted into All bold and italic text ...");
+		modifyText("*** ***");
+	});
+
+
+	let quoting = vscode.commands.registerCommand('codebump.quoting', () => {
+		vscode.window.showInformationMessage("Formatted into quoting text ...");
+		modifyText(">");
+	});
+
 
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+	context.subscriptions.push(largestHeading);
+	context.subscriptions.push(secondHeading);
+	context.subscriptions.push(smallHeading);
+	
+	context.subscriptions.push(boldText);
+	context.subscriptions.push(italic);
+	context.subscriptions.push(strikethrough);
+	context.subscriptions.push(boldItalic);
+	context.subscriptions.push(allBoldItalic);
+
+	context.subscriptions.push(quoting);
+	
+
+
+
+
+
 }
 
 
